c41becfb0bbc776818440e38c96d20961ed68056
staletoast <melody.trojanowski@education.nsw.gov.au>
2023-08-23 09:42:08 +1000

    completed activity 10

--- 10.servoMotor/10.servoMotor.ino
+++ 10.servoMotor/10.servoMotor.ino
@@ -1,5 +1,5 @@
 /*
-  Author: 
+  Author: Melody Trojanowski
   Learning Intention: The students will learn how to connect and control a servo motor.
   Success Criteria:
     1. I understand how to connect the servo motor
@@ -20,12 +20,40 @@
 
 //The Servo-h library is built into Ardunio IDE there is no need to side load it like the Ultrasonic Sensor library
 
+#include <Servo.h>
+
+//create servo object
+Servo myServo;
+
+//pin servo is attached to
+unsigned static int myServoPin = 8;
 
 void setup() {
+//setup SM
+Serial.begin(9600);
+Serial.println("Serial Monitor configured to 9600");
+Serial.println("...............................");
 
+//config servo to pin
+myServo.attach(myServoPin);
+myServo.write(0);
+delay (250);
 }
 
 // The loop function runs over and over again forever
 void loop() {
 
+
+//sweep to 180
+for (int i = 0; i <= 180; i++) {
+myServo.write(i);
+delay(50);
+
+}
+
+//sweep to 180
+for (int y = 180; y <= 180; y--) {
+myServo.write(y);
+delay(50);
+}
 }

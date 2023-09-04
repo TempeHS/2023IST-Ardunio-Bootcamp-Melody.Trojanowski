c1560a5f06d71cead13fc2db607132aabdcf1927
staletoast <melody.trojanowski@education.nsw.gov.au>
2023-09-04 14:21:46 +1000

    commits to activity 13.12C

---
+++ 13.I2C/functions.ino
@@ -0,0 +1,8 @@
+void printToOLED(String theMessage, String theMessage2) {
+  u8g2.clearBuffer();                   // clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr);   // choose a suitable font
+  u8g2.drawStr(0,10, theMessage.c_str());    // write something to the internal memory
+  u8g2.drawStr(0,10, theMessage2.c_str());    // write something to the internal memory
+  u8g2.sendBuffer();                    // transfer internal memory to the display
+  delay(1000);  
+}
